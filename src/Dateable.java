f172a304c07942c8adff387af9ebefa72e55a645
techarenz <57818506+techarenz@users.noreply.github.com>
2023-08-31 11:53:19 +0800

    Update README.md

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner userAge = new Scanner(System.in); //userAge will be what the user types
+        System.out.print("How old are you?"); //Asks user how old they are
+        int answer = userAge.nextInt(); //Takes the answer (recognizes it as a integer)
+        int dateAge = answer/2+7; //Calculate ideal dating age
+        System.out.println(answer + "-year olds should date somebody who is at least " + dateAge + " years old."); //Output after calculation
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner userAge = new Scanner(System.in); //userAge will be what the user types
+        System.out.println("Enter your age."); //Asks user how old they are
+        int input = userAge.nextInt(); //Takes the answer (recognizes it as a integer)
+        int output = 100-input; //Calculate ideal dating age
+        System.out.println(input + "-year olds should read at least " + output + " pages before giving up on a book."); //Output after calculation
 
     }
 }
